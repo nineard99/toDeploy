3c9cda785ed6bc2d0094d6015b5c0c47965e4590
Jitkal Mitrakul <66130500113@ad.sit.kmutt.ac.th>
2025-01-25 13:48:48 +0700

    113-[update]loop of deck & rearrange deck

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,30 @@
 <script setup>
     import { ref } from 'vue'
 
-    let arrDeck=[
-        {'symbols':'club','number':'K'},{'symbols':'club','number':'Q'},{'symbols':'club','number':'J'},
-        {'symbols':'diamond','number':'K'},{'symbols':'diamond','number':'Q'},{'symbols':'diamond','number':'J'},
-        {'symbols':'heart','number':'K'},{'symbols':'heart','number':'Q'},{'symbols':'heart','number':'J'},
-        {'symbols':'spade','number':'K'},{'symbols':'spade','number':'Q'},{'symbols':'spade','number':'J'}
-    ]
+    let arrDeck=[]
     for (let i = 1; i <= 4; i++) {
+        let symbols
         switch (i) {
             case 1:
-                for (let j = 1; j <= 10; j++) {
-                    arrDeck.push({'symbols':'club','number':j})
-                }
+                symbols='club'
                 break;
             case 2:
-                for (let j = 1; j <= 10; j++) {
-                    arrDeck.push({'symbols':'diamond','number':j})
-                }
+                symbols='diamond'
                 break;
             case 3:
-                for (let j = 1; j <= 10; j++) {
-                    arrDeck.push({'symbols':'heart','number':j})
-                }
+                symbols='heart'
                 break;
             case 4:
-                for (let j = 1; j <= 10; j++) {
-                    arrDeck.push({'symbols':'spade','number':j})
-                }
+                symbols='spade'
                 break;
         }
+        arrDeck.push({'symbols':symbols,'number':'K','values':10})
+        arrDeck.push({'symbols':symbols,'number':'Q','values':10})
+        arrDeck.push({'symbols':symbols,'number':'J','values':10})
+        for (let j = 10; j >= 2; j--) {
+            arrDeck.push({'symbols':symbols,'number':j,'values':j})
+        }
+        arrDeck.push({'symbols':symbols,'number':'A','values':1})
     }
    
     //Check BlackJack Player
@@ -71,7 +66,7 @@
 
 <template>
     <div class="test flex-col">
-        <div id="deck" v-show="false" class="grid grid-cols-12 gap-4">
+        <div id="deck" v-show="true" class="grid grid-cols-12 gap-4">
             <div v-for="card in arrDeck">
                 <div v-if="card.symbols=='club'||card.symbols=='spade'" class="w-40 h-48 border-2 border-solid border-gray-900 rounded-sm bg-stone-100 flex flex-col justify-between">
                     <div style="padding: 10px;"><img :src="`../Image/${card.symbols}.png`" style="width:25px;"></div>
@@ -80,7 +75,7 @@
                 </div>
                 <div v-else="card.symbols=='diamond'||card.symbols=='heart'" class="w-40 h-48 border-2 border-solid border-gray-900 rounded-sm bg-rose-50 flex flex-col justify-between">
                     <div style="padding: 10px;"><img :src="`../Image/${card.symbols}.png`" style="width:25px;"></div>
-                    <div style="text-align: center; font-size: 70px; color: #282828;font-family: Oldenburg;">{{ card.number }}</div>
+                    <div style="text-align: center; font-size: 70px; color: #ff4d4d;font-family: Oldenburg;">{{ card.number }}</div>
                     <div style="padding: 10px; rotate: 180deg; align-items: end;"><img :src="`../Image/${card.symbols}.png`" style="width:25px;"></div>
                 </div>
             </div>
