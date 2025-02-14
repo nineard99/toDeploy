318aa335a6be4e1f8efcc614fe229d2f277d7d0a
Jitkal Mitrakul <66130500113@ad.sit.kmutt.ac.th>
2025-02-14 21:13:31 +0700

    113-[update]arrDeck

--- src/scripts/deck.js
+++ src/scripts/deck.js
@@ -4,32 +4,27 @@ let usedCards=[]
 
 for (let i = 1; i <= 4; i++) {
     let symbols
-    let color
     switch (i) {
         case 1:
             symbols='club'
-            color='gray-100'
             break;
         case 2:
             symbols='diamond'
-            color='rose-50'
             break;
         case 3:
             symbols='heart'
-            color='rose-50'
             break;
         case 4:
             symbols='spade'
-            color='gray-100'
             break;
     }
-    arrDeck.push({symbols:symbols,number:'K',values:10,color:color})
-    arrDeck.push({symbols:symbols,number:'Q',values:10,color:color})
-    arrDeck.push({symbols:symbols,number:'J',values:10,color:color})
+    arrDeck.push({symbols:symbols,number:'K',values:10})
+    arrDeck.push({symbols:symbols,number:'Q',values:10})
+    arrDeck.push({symbols:symbols,number:'J',values:10})
     for (let j = 10; j >= 2; j--) {
-        arrDeck.push({'symbols':symbols,'number':j,'values':j,color:color})
+        arrDeck.push({'symbols':symbols,'number':j,'values':j})
     }
-    arrDeck.push({'symbols':symbols,'number':'A','values':1,color:color})
+    arrDeck.push({'symbols':symbols,'number':'A','values':1})
 }
 
 
